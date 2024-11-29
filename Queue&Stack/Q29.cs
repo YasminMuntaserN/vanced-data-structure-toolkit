ecfb6064656206f198741d99129ab04838e0a414
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-29 10:10:49 +0200

    feat: add question 29 and 30 to stack , queue questions

---
+++ Queue&Stack/Q29.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Stack
+{
+    /*
+     Problem: Evaluate a mathematical expression containing +, -, (, ) without * or /.
+
+    Example:
+    Input: "1 + (2 - 3)"
+    Output: 0
+
+    Key Points:
+    Use a stack to handle parentheses and maintain the current sum.
+     */
+    public class Q29
+    {
+        public static int EvaluateExpression(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            int number = 0;
+            char sign = '+';
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = s[i];
+
+                if (char.IsDigit(ch))
+                {
+                    number = int.Parse(ch.ToString());
+                }
+
+                if (ch == '(')
+                {
+                    int j = i, brackets = 1;
+                    while (brackets > 0)
+                    {
+                        i++;
+                        if (s[i] == '(') brackets++;
+                        if (s[i] == ')') brackets--;
+                    }
+                    string sub = s.Substring(j + 1, i - j - 1);
+                    number = EvaluateExpression(sub); 
+                }
+
+                if (!char.IsDigit(ch) && ch != ' ' || i == s.Length - 1)
+                {
+                    if (sign == '+') stack.Push(number);
+                    else if (sign == '-') stack.Push(-number);
+
+                    sign = ch;
+                    number = 0;
+                }
+            }
+
+            int result = 0;
+            while (stack.Count > 0) result += stack.Pop();
+            return result;
+        }
+
+        public static void  Question()
+        {
+            Console.WriteLine($"1 + (2 - 3) = { EvaluateExpression("1 + (2 - 3)")}");
+        }
+    }
+
+}
---
+++ Queue&Stack/Q30.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Stack
+{
+    /*
+     Problem: Remove the minimum number of invalid parentheses to make the string valid.
+
+    Example:
+    Input: "(()))"
+    Output: "()" or "(())"
+
+    Key Points:
+    Use a stack to track mismatched parentheses.
+     */
+    public class Q30
+    {
+        /*  private static string InvalidParentheses(string s)
+          {
+              StringBuilder result = new();
+              Stack<char> stack = new Stack<char>();
+              foreach (char c in s)
+              {
+                  if (c == '(') stack.Push('(');
+
+                  if (c == ')' && stack.Contains('('))
+                   {
+                      result.Append(stack.Pop());
+                  }
+              }
+              foreach (var c in result.ToString()) result.Append(')');
+              return result.ToString();
+          }*/
+        static string RemoveInvalidParentheses(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            HashSet<int> invalidIndices = new HashSet<int>();
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (s[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        invalidIndices.Add(i);
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+
+            while (stack.Count > 0)
+            {
+                invalidIndices.Add(stack.Pop());
+            }
+
+
+            char[] result = new char[s.Length - invalidIndices.Count];
+            int index = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!invalidIndices.Contains(i))
+                {
+                    result[index++] = s[i];
+                }
+            }
+
+
+            return new string(result);
+        }
+
+        public static void Question()
+        {
+           // Console.WriteLine($"(())) => {InvalidParentheses("(()))")}");
+           // Console.WriteLine($"(())) => {InvalidParentheses("(()))")}");
+
+            Console.WriteLine($"(())) => {RemoveInvalidParentheses("(()))")}");
+            Console.WriteLine($"(())) => {RemoveInvalidParentheses("(()))")}");
+        }
+    }
+}
